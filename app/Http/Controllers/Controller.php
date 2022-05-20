7a9ad4859acbe5840dc665456038510b9d2f7d7c
michele camera <michy.cam95@gmail.com>
2022-05-20 14:37:53 +0200

    modify controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,13 +6,8 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use App\Movie;
+
 
 class Controller extends BaseController
 {
-    public function index()
-    {
-        $movies = Movie::all();
-        return view('home', $movies);
-    }
 }
