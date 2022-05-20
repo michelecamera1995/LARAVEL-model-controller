44d97423abf1db2d6598ff4ea98e19533059c146
michele camera <michy.cam95@gmail.com>
2022-05-20 14:16:40 +0200

    modify load  database

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,13 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Movie;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function index()
+    {
+        $movies = Movie::all();
+        return view('welcome', $movies);
+    }
 }
